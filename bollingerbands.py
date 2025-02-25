2113407ea58c97641b62d98ee547359c75405ae4
daniel <daniel891116@gmail.com>
2025-02-25 17:03:03 +0800

    視覺化

--- bollingerbands.py
+++ bollingerbands.py
@@ -35,5 +35,45 @@ data['Sell_Signal'] = (data['Sideways_Market']) & (data['RSI'] > 70) & (data['K'
 data['Trend_Buy'] = (data['Trend_Market']) & (data['MACD'] > data['Signal']) & (data['RSI'] > 50) & (data['K'] > data['D'])
 data['Trend_Sell'] = (data['Trend_Market']) & (data['MACD'] < data['Signal']) & (data['RSI'] < 50) & (data['K'] < data['D'])
 
-# 顯示結果
-print(data[['Close', 'BB_Width', 'MACD', 'Signal', 'RSI', 'K', 'D', 'Trend_Market', 'Sideways_Market', 'Buy_Signal', 'Sell_Signal', 'Trend_Buy', 'Trend_Sell']].tail(10))
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+# import plotly.graph_objects as go
+import numpy as np
+
+# 繪製股價與布林通道
+fig, ax = plt.subplots(3, 1, figsize=(14, 12), sharex=True)
+
+# 1. 股價與布林通道
+ax[0].plot(data.index, data['Close'], label="Close Price", color='blue', linewidth=1)
+ax[0].plot(data.index, data['Upper_Band'], label="Upper Band", color='red', linestyle='--', linewidth=1)
+ax[0].plot(data.index, data['Middle_Band'], label="Middle Band", color='green', linestyle='-', linewidth=1)
+ax[0].plot(data.index, data['Lower_Band'], label="Lower Band", color='red', linestyle='--', linewidth=1)
+ax[0].fill_between(data.index, data['Lower_Band'], data['Upper_Band'], color='gray', alpha=0.3)
+ax[0].set_title(f"{ticker} Stock Price & Bollinger Bands")
+ax[0].set_ylabel("Price")
+ax[0].legend()
+
+# 2. MACD 和 Signal Line
+ax[1].plot(data.index, data['MACD'], label="MACD", color='blue', linewidth=1)
+ax[1].plot(data.index, data['Signal'], label="Signal Line", color='red', linestyle='--', linewidth=1)
+ax[1].bar(data.index, data['MACD'] - data['Signal'], label="MACD Histogram", color='green', alpha=0.3)
+ax[1].set_title(f"{ticker} MACD & Signal Line")
+ax[1].set_ylabel("Value")
+ax[1].legend()
+
+# 3. RSI
+ax[2].plot(data.index, data['RSI'], label="RSI", color='purple', linewidth=1)
+ax[2].axhline(30, color='green', linestyle='--', linewidth=1, label="30 - Oversold")
+ax[2].axhline(70, color='red', linestyle='--', linewidth=1, label="70 - Overbought")
+ax[2].set_title(f"{ticker} RSI")
+ax[2].set_ylabel("RSI Value")
+ax[2].legend()
+
+# 格式設置
+ax[2].set_xlabel("Date")
+ax[2].xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+ax[2].xaxis.set_major_locator(mdates.MonthLocator())
+fig.autofmt_xdate()
+
+plt.tight_layout()
+plt.show()
